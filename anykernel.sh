bb1445318a3f8f853ac128def9c99e9473f7833f
AliothProject <88237000+DawFuk@users.noreply.github.com>
2022-06-04 16:14:08 +0200

    Update : Support Oneplus 8t

--- anykernel.sh
+++ anykernel.sh
@@ -9,11 +9,11 @@ do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=alioth
-device.name2=aliothin
-device.name3=apollo
-device.name4=apollon
-device.name5=apollopro
+device.name1=kebab
+device.name2=
+device.name3=
+device.name4=
+device.name5=
 supported.versions=
 supported.patchlevels=
 '; } # end properties
